e430f04b63300e3f6a55dc0ea8c0244a738ce1a0
罗凯#A02637 <346195459@qq.com>
2022-09-17 17:07:43 +0800

    u

--- src/TipsBanner/dist/example.js
+++ src/TipsBanner/dist/example.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import TipsBanner from ".";
 export default function App() {
-    return _jsx(TipsBanner, Object.assign({ onClose: () => console.log("close.") }, { children: "\u8BF7\u52FF\u968F\u5730\u5927\u5C0F\u4FBF\uFF01" }), void 0);
+    return _jsx(TipsBanner, { onClose: () => console.log("close."), children: "\u8BF7\u52FF\u968F\u5730\u5927\u5C0F\u4FBF\uFF01" });
 }
--- src/TipsBanner/dist/index.js
+++ src/TipsBanner/dist/index.js
@@ -4,5 +4,5 @@ import close from "../assets/close.svg";
 import "../assets/index.css";
 /** 横幅形式的消息提醒 */
 export default function TipsBanner({ children, onClose }) {
-    return (_jsxs("div", Object.assign({ className: "rc-tips-banner" }, { children: [_jsx("img", { src: info, alt: "" }, void 0), _jsx("div", Object.assign({ className: "rc-tips-banner-content" }, { children: children }), void 0), !!onClose && _jsx("img", { className: "rc-tips-banner-close", src: close, alt: "", onClick: onClose }, void 0)] }), void 0));
+    return (_jsxs("div", { className: "rc-tips-banner", children: [_jsx("img", { src: info, alt: "" }), _jsx("div", { className: "rc-tips-banner-content", children: children }), !!onClose && _jsx("img", { className: "rc-tips-banner-close", src: close, alt: "", onClick: onClose })] }));
 }
